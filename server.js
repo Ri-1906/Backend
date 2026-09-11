a31f033d467877ece9282d91d6136069e8f57d28
Ri-1906 <riddhi030304@gmail.com>
2026-09-11 17:08:18 +0530

    updated server

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
-// server ko create krna   (root mein)
-const app = require("./src/app")
+require("dotenv").config()
+const app = require('./src/app')
+const connectdb = require('./src/db/db')
 
-app.listen(3000,()=>{
-    console.log("server is running on port 3000.")
+connectdb()
+
+
+
+
+app.listen(3000,()=> {
+    console.log("Server is running on port 3000.")
 })
